cdbd7cf107ac456cdd2ab1d8e4370e121223c0b8
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-04 18:05:52 +0530

    fix

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const tiny = require("tiny-json-http");
+module.exports = async function(i, t) {
+    let d = "https://www.google.com/search?q={artist}+{title}+lyrics";
+    d = (d = await tiny.get({
+        url: d
+    })).body;
+    try {
+        d = d.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">')[1].split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">')[0]
+    } catch (i) {
+        d = ""
+    }
+    return d
+};
