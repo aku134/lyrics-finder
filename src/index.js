47b5fb1b929b321a04d410def37dc4df8ebc6a2a
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-05 13:53:13 +0530

    fixed readme

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const tiny = require("tiny-json-http"),
       urlencode = require("urlencode"),
-      unidecode = require("unidecode");
+      unidecode = require("unidecode-plus");
 module.exports = async function(i, t) {
     let d = `https://www.google.com/search?q=${urlencode(t+ ' ' +i)}+lyrics`;
-    d = (d = await tiny.get({
+    d = unidecode((d = await tiny.get({
         url: d
-    })).body;
+    })).body);
     try {
         d = d.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">')[1].split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">')[0]
     } catch (i) {
         d = ""
     }
-    return unidecode(d)
+    return d
 };
