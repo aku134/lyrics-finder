878871f22045371aa6a5a5a4552df9ebeb3b16b1
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-05 16:46:14 +0530

    changed dependency

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const tiny = require("tiny-json-http"),
-    urlencode = require("urlencode"),
-    unidecode = require("unidecode-plus");
+const fetch = require("node-fetch"),
+    urlencode = require("urlencode");
 module.exports = async function(e, d) {
-    let i = `https://www.google.com/search?q=${urlencode(d+" "+e)}+lyrics`;
-    i = unidecode((i = await tiny.get({
-        url: i
-    })).body);
+    let i = await fetch(`https://www.google.com/search?q=${urlencode(d+" "+e)}+lyrics`);
+    i = await i.text();
     try {
         i = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">')[1].split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">')[0]
     } catch (e) {
