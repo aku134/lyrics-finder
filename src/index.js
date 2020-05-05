c69ef0a6ab77887f19e6f4312ca4ad282b36ba58
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-05 13:56:34 +0530

    minified script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const tiny = require("tiny-json-http"),
-      urlencode = require("urlencode"),
-      unidecode = require("unidecode-plus");
-module.exports = async function(i, t) {
-    let d = `https://www.google.com/search?q=${urlencode(t+ ' ' +i)}+lyrics`;
-    d = unidecode((d = await tiny.get({
-        url: d
+    urlencode = require("urlencode"),
+    unidecode = require("unidecode-plus");
+module.exports = async function(e, d) {
+    let i = `https://www.google.com/search?q=${urlencode(d+" "+e)}+lyrics`;
+    i = unidecode((i = await tiny.get({
+        url: i
     })).body);
     try {
-        d = d.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">')[1].split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">')[0]
-    } catch (i) {
-        d = ""
+        i = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">')[1].split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">')[0]
+    } catch (e) {
+        i = ""
     }
-    return d
+    return i
 };
