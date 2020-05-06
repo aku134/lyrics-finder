128af0dbb2bca7edbf96d3971654dd09513404cc
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-06 13:20:13 +0530

    update

--- test/test.js
+++ test/test.js
@@ -3,6 +3,19 @@ const shelljs = require('shelljs');
 const fs = require('fs');
 const lyricsFinder = require('../src/index.js');
 
+describe('console.log()', () => {
+  it('for "shakira", "chantaje" should not be equal to "Not Found!"', () => {
+    shelljs.exec("./src/bin.js 'shakira' 'chantaje' > ./src/output.txt");
+    expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
+    shelljs.exec('rm ./src/output.txt');
+  });
+  it('for "dream theatre", "another day" should not be equal to "Not Found!"', () => {
+    shelljs.exec("./src/bin.js 'dream theatre' 'another day' > ./src/output.txt");
+    expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
+    shelljs.exec('rm ./src/output.txt');
+  });
+});
+
 describe('lyricsFinder()', () => {
   it('for "shakira", "chantaje" should be of length greater than zero', async () => {
     expect((await lyricsFinder('shakira', 'chantaje')).length > 0).to.be.true;
@@ -15,18 +28,5 @@ describe('lyricsFinder()', () => {
   });
   it('for "dream theatre", "another day" should be of length greater than zero', async () => {
     expect((await lyricsFinder('dream theatre', 'another day')).length > 0).to.be.true;
-  });
-});
-
-describe('console.log()', () => {
-  it('for "shakira", "chantaje" should not be equal to "Not Found!"', () => {
-    shelljs.exec("./src/bin.js 'shakira' 'chantaje' > ./src/output.txt");
-    expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
-    shelljs.exec('rm ./src/output.txt');
-  });
-  it('for "dream theatre", "another day" should not be equal to "Not Found!"', () => {
-    shelljs.exec("./src/bin.js 'dream theatre' 'another day' > ./src/output.txt");
-    expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
-    shelljs.exec('rm ./src/output.txt');
   });
 });
