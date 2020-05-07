e7b3768c329b53fb5ea83d3385ba6ad1e7b63c54
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-07 11:59:50 +0530

    update

--- test/test.js
+++ test/test.js
@@ -6,11 +6,6 @@ const fs = require('fs');
 const lyricsFinder = require('../src/index.js');
 
 describe('console.log()', () => {
-  it('for "shakira", "chantaje" should not be equal to "Not Found!"', () => {
-    shelljs.exec("./src/bin.js 'shakira' 'chantaje' > ./src/output.txt");
-    expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
-    shelljs.exec('rm ./src/output.txt');
-  });
   it('for "dream theatre", "another day" should not be equal to "Not Found!"', () => {
     shelljs.exec("./src/bin.js 'dream theatre' 'another day' > ./src/output.txt");
     expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
@@ -21,8 +16,5 @@ describe('console.log()', () => {
 describe('lyricsFinder()', () => {
   it('for "shakira", "chantaje" should be of length greater than zero', async () => {
     expect((await lyricsFinder('shakira', 'chantaje')).length > 0).to.be.true;
-  });
-  it('for "dream theatre", "another day" should be of length greater than zero', async () => {
-    expect((await lyricsFinder('dream theatre', 'another day')).length > 0).to.be.true;
   });
 });
