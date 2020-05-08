18f1406742323b10835315b894e5d736d6afaba2
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-08 16:21:07 +0530

    update

--- src/index.js
+++ src/index.js
@@ -49,12 +49,12 @@ async function main(e, d) {
     } catch (m) {
       try {
         if ([true, false][Math.floor(Math.random() * 2)]) {
-          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics+song`);
+          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song+lyrics`);
           i = await i.text();
           [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
           [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
         } else {
-          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song+lyrics`);
+          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics+song`);
           i = await i.text();
           [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
           [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
