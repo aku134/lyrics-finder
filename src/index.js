a6265deed0f6e28f9d313b0c172191df545d10c4
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-08 16:14:21 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch');
 const urlencode = require('urlencode');
 
 async function main(e, d) {
+  let i;
   if ([true, false][Math.floor(Math.random() * 2)]) {
-    let i;
     try {
       i = await fetch(`https://www.google.com/search?q=${urlencode(`${d} ${e}`)}+lyrics`);
       i = await i.text();
@@ -40,41 +40,40 @@ async function main(e, d) {
         }
       }
     }
-    return i;
-  }
-  let i;
-  try {
-    i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics`);
-    i = await i.text();
-    [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
-    [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
-  } catch (m) {
+  } else {
     try {
-      if ([true, false][Math.floor(Math.random() * 2)]) {
-        i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics+song`);
-        i = await i.text();
-        [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
-        [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
-      } else {
-        i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song+lyrics`);
-        i = await i.text();
-        [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
-        [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
-      }
-    } catch (n) {
+      i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics`);
+      i = await i.text();
+      [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
+      [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
+    } catch (m) {
       try {
-        i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song`);
-        i = await i.text();
-        [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
-        [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
-      } catch (o) {
-        try {
-          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}`);
+        if ([true, false][Math.floor(Math.random() * 2)]) {
+          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+lyrics+song`);
           i = await i.text();
           [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
           [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
-        } catch (p) {
-          i = '';
+        } else {
+          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song+lyrics`);
+          i = await i.text();
+          [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
+          [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
+        }
+      } catch (n) {
+        try {
+          i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}+song`);
+          i = await i.text();
+          [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
+          [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
+        } catch (o) {
+          try {
+            i = await fetch(`https://www.google.com/search?q=${urlencode(`${e} ${d}`)}`);
+            i = await i.text();
+            [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
+            [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
+          } catch (p) {
+            i = '';
+          }
         }
       }
     }
