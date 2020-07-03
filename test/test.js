7084c56593f99bdaf5a41f68f39ee4e9963e8ad3
Your Name <you@example.com>
2020-07-03 11:57:35 +0530

    update

--- test/test.js
+++ test/test.js
@@ -8,14 +8,14 @@ const lyricsFinder = require('../src/index.js');
 describe('lyricsFinder()', function () {
   this.timeout(0);
   it('for "shakira", "chantaje" should be of length greater than zero', async () => {
-    shelljs.exec("./src/bin.js 'dream theatre' 'another day' > ./src/output.txt");
     expect((await lyricsFinder('shakira', 'chantaje')).length > 0).to.be.true;
   });
 });
-
 describe('console.log()', function () {
   this.timeout(0);
   it('for "dream theatre", "another day" should not be equal to "Not Found!"', () => {
+    shelljs.exec("./src/bin.js 'dream theatre' 'another day' > ./src/output.txt");
     expect(String(fs.readFileSync('./src/output.txt')).trim() !== 'Not Found!').to.be.true;
+    fs.unlinkSync('./src/output.txt');
   });
 });
