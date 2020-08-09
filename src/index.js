d0230087f936224d71fc89c3a119431a3ad3528e
Your Name <you@example.com>
2020-08-09 18:41:38 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 const fetch = require('node-fetch');
 const urlencode = require('urlencode');
 const htmlToText = require('html-to-text');
-const unidecode = require('unidecode');
+const encoding = require('encoding');
 
 async function main(e, d) {
   let i;
   try {
     i = await fetch(`https://www.google.com/search?q=${urlencode(`${d} ${e}`)}+lyrics`);
-    i = await i.text();
+    i = await i.textConverted();
     [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
     [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
   } catch (m) {
     try {
       i = await fetch(`https://www.google.com/search?q=${urlencode(`${d} ${e}`)}+song+lyrics`);
-      i = await i.text();
+      i = await i.textConverted();
       [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
       [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
     } catch (n) {
       try {
         i = await fetch(`https://www.google.com/search?q=${urlencode(`${d} ${e}`)}+song`);
-        i = await i.text();
+        i = await i.textConverted();
         [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
         [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
       } catch (o) {
         try {
           i = await fetch(`https://www.google.com/search?q=${urlencode(`${d} ${e}`)}`);
-          i = await i.text();
+          i = await i.textConverted();
           [, i] = i.split('</div></div></div></div><div class="hwc"><div class="BNeawe tAd8D AP7Wnd"><div><div class="BNeawe tAd8D AP7Wnd">');
           [i] = i.split('</div></div></div></div></div><div><span class="hwc"><div class="BNeawe uEec3 AP7Wnd">');
         } catch (p) {
@@ -37,8 +37,8 @@ async function main(e, d) {
   const ret = i.split('\n');
   let final = '';
   for (let j = 0; j < ret.length; j += 1) {
-    final = `${final}\n${unidecode(htmlToText.fromString(ret[j]))}`;
+    final = `${final}${htmlToText.fromString(ret[j])}\n`;
   }
-  return final.trim();
+  return String(encoding.convert(final)).trim();
 }
 module.exports = main;
